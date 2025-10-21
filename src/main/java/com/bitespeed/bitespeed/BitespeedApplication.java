68cf2eb7553ac5e1d9af9ad1d256be0f43b3d2b8
Sandeep Bhanja <74126755+sandeepbhanja@users.noreply.github.com>
2025-10-21 17:16:10 +0530

    Update BitespeedApplication.java

--- src/main/java/com/bitespeed/bitespeed/BitespeedApplication.java
+++ src/main/java/com/bitespeed/bitespeed/BitespeedApplication.java
@@ -1,13 +1,22 @@
 package com.bitespeed.bitespeed;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BitespeedApplication {
+public class BitespeedApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BitespeedApplication.class, args);
-	}
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
+    public static void main(String[] args) {
+        SpringApplication.run(BitespeedApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("JDBC URL: " + dbUrl);
+    }
 }
